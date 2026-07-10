8eb471e8336e8fda407a334465fb8386640e4a88
Matt Ryan <7944764+MattRyanCo@users.noreply.github.com>
2026-07-10 09:22:58 -0400

    Rename functions and update author details

--- lib/functions/general.php
+++ lib/functions/general.php
@@ -6,8 +6,8 @@
  *
  * @package      Core_Functionality
  * @since        1.0.0
- * @link         https://github.com/capwebsolutions/capwebwpcare-core-functionality
- * @author       Matt Ryan <matt@capwebsolutions.com>
+ * @link         https://github.com/mattryanco/mrwpcare-core-functionality
+ * @author       Matt Ryan <matt@mattryan.co>
  * @copyright    Copyright (c) 2017, Matt Ryan
  * @license      http://opensource.org/licenses/gpl-2.0.php GNU Public License
  */
@@ -21,7 +21,7 @@
  * @param array $menu_ord. Current order.
  * @return array $menu_ord. New order.
  */
-function capweb_custom_menu_order( $menu_ord ) {
+function mrco_custom_menu_order( $menu_ord ) {
 	if ( ! $menu_ord ) { return true;
 	}
 	return array(
@@ -32,15 +32,15 @@ function capweb_custom_menu_order( $menu_ord ) {
 		'upload.php', // the media manager
 	);
 }
-add_filter( 'custom_menu_order', 'capweb_custom_menu_order' );
-add_filter( 'menu_order', 'capweb_custom_menu_order' );
+add_filter( 'custom_menu_order', 'mrco_custom_menu_order' );
+add_filter( 'menu_order', 'mrco_custom_menu_order' );
 
 // Keep MainWP site user logged in all the time.
-function extend_auth_cookie_expiration( $expiration, $user_id, $remember ) {
+function mrco_extend_auth_cookie_expiration( $expiration, $user_id, $remember ) {
     // Set the desired cookie expiration time
     $cookie_expire = 2 * MONTH_IN_SECONDS;
 
     // Return the new expiration time
     return $cookie_expire;
 }
-add_filter( 'auth_cookie_expiration', 'extend_auth_cookie_expiration', 10, 3 );
+add_filter( 'auth_cookie_expiration', 'mrco_extend_auth_cookie_expiration', 10, 3 );
